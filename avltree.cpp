063f7b1f7797224e2409d95e0d3dd052f27e4089
huangyingw@linux_programming <huangyingw@gmail.com>
2010-04-08 03:08:17 -0400

    n

---
+++ avltree.cpp
@@ -0,0 +1,23 @@
+#include "avltree.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	AVLTree *avlTree=new AVLTree();
+	int taller=1;
+	int shorter=1;
+	avlTree->Insert(avlTree->root,16,taller);
+	avlTree->Insert(3);
+	avlTree->Insert(7);
+	avlTree->Insert(11);
+	avlTree->Insert(9);
+	avlTree->Insert(26);
+	avlTree->Insert(18);
+	avlTree->Insert(14);
+	avlTree->Insert(15);
+	avlTree->PrintAVLHor(avlTree->root,cout);
+
+	avlTree->Remove(avlTree->root,18,shorter);
+	avlTree->PrintAVLHor(avlTree->root,cout);
+	return 0;
+}
+
